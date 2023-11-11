75e4e399a396aa328d5a4fc5865497ba388b3ba5
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-11-11 08:15:40 +0530

    product detail 200vh

--- Client/src/Components/ProductDetail/ProductDetail.jsx
+++ Client/src/Components/ProductDetail/ProductDetail.jsx
@@ -50,9 +50,37 @@ function ProductDetail() {
               <div className="tags"></div>
           </div>
       </div>
-      {/* <div className='second_100_s'>
-
-      </div> */}
+      <div className='second_100_s'>
+          <div className="lefts_s">
+            <div className="headingprod_s">
+              <h1 style={{color:"red"}}>Product Information:</h1>
+            </div>
+            <div className="flex" style={{display:"flex",width:"100%",height:"100%"}}>
+            <div className="prodinfo1_s">
+              <h2 className='key'>Product Name:</h2>
+              <h2 className='key'>Quantity:</h2>
+              <h2 className='key'>Rate:</h2>
+              <h2 className='key'>Shop Number:</h2>
+              <h2 className='key'>Shop Address:</h2>
+            </div>
+            <div className="prodinfo2_s">
+              <h2 className='value'>Laptop</h2>
+              <h2 className='value'>10</h2>
+              <h2 className='value'>10000</h2>
+              <h2 className='value'>9944012249</h2>
+              <h2 className='value'>chennai ,tambaram ,busstand road</h2>
+            </div>
+            </div>
+          </div>
+          <div className="rights_s">
+            <h1 style={{color:"red"}}>Description:</h1>
+            <h3 style={{opacity:"0.7"}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque possimus aut neque adipisci perferendis atque tempora qui architecto nulla dolorum repudiandae optio hic dolore blanditiis aperiam eaque, voluptas voluptates placeat.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, incidunt esse tempore dignissimos nemo odit sit tenetur ipsum, nisi harum cupiditate repellat praesentium reprehenderit? Sunt omnis fuga consequuntur cum quis?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iure voluptas eligendi quae debitis error, odit cumque minima repellendus optio dolor fugit veniam alias expedita dolore aperiam veritatis illo. Beatae.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti a adipisci eum atque cumque officia minima eius. Totam ratione eos facilis recusandae optio, eligendi nam, exercitationem quae itaque consectetur unde.
+            </h3>
+          </div>
+      </div>
     </div>
   </div>
   )
